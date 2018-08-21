636f2935348a11db8ba17a87edbd333984fd8714
Anatoly Mozgov <Anatoly_Mozgov@rcomext.com>
2018-08-21 15:30:58 +0300

    Added back-off if there's a bean of type `J9HeapdumpMvcEndpoint` or `HeapdumpMvcEndpoint` provided by user.
    Also, made the `J9HeapdumpAutoconfiguration` work same as the other Actuator Autoconfigurations, honouring the `management.server.port` setting (see https://github.com/spring-projects/spring-boot/issues/7616#issuecomment-267729645 for details)

--- ibm-heapdump-spring-boot-actuator-autoconfigure/src/main/java/io/github/sa1nt/ibmheapdump/autoconfiguration/J9HeapdumpAutoconfiguration.java
+++ ibm-heapdump-spring-boot-actuator-autoconfigure/src/main/java/io/github/sa1nt/ibmheapdump/autoconfiguration/J9HeapdumpAutoconfiguration.java
@@ -1,20 +1,33 @@
 package io.github.sa1nt.ibmheapdump.autoconfiguration;
 
 import io.github.sa1nt.ibmheapdump.J9HeapdumpMvcEndpoint;
+import org.springframework.boot.actuate.autoconfigure.ManagementContextConfiguration;
 import org.springframework.boot.actuate.condition.ConditionalOnEnabledEndpoint;
+import org.springframework.boot.actuate.endpoint.mvc.HeapdumpMvcEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
-@Configuration
+/**
+ * Configuration to inject a {@link J9HeapdumpMvcEndpoint} instead of the default {@link HeapdumpMvcEndpoint} if the
+ * /heapdump Actuator endpoint is enabled.
+ * <p>
+ * Ordered to run with highest priority to override the default
+ * {@link org.springframework.boot.actuate.autoconfigure.EndpointWebMvcManagementContextConfiguration}.
+ * <p>
+ * Will back off if a bean of type {@link HeapdumpMvcEndpoint} or {@link J9HeapdumpMvcEndpoint} is provided by user code.
+ */
+@ManagementContextConfiguration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @ConditionalOnClass(J9HeapdumpMvcEndpoint.class)
 @ConditionalOnEnabledEndpoint("heapdump")
 public class J9HeapdumpAutoconfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
-    public J9HeapdumpMvcEndpoint heapdumpMvcEndpoint() {
+    @ConditionalOnMissingBean({J9HeapdumpMvcEndpoint.class, HeapdumpMvcEndpoint.class})
+    public HeapdumpMvcEndpoint heapdumpMvcEndpoint() {
         return new J9HeapdumpMvcEndpoint();
     }
 }
